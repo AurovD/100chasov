7b0f8c0f4066c73680fb117328cde451d0ef3495
Aurov Dmitry <dmitry8891@mail.ru>
2025-01-03 18:45:58 +0300

    tanstackquery, usemutation, login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import {Popup} from "../components/UI/Popup";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (<><Component {...pageProps} /><Popup/></>)
+  return (<>
+    <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+    <Popup/>
+    </QueryClientProvider>
+  </>)
 }
 
 export default MyApp
